6188716f71a6273bc9ce5ab88e0ec318eaf69d66
Dhiego Vieira <dhiego.vieira@mercadolivre.com>
2024-08-16 17:36:47 -0300

    fixed

--- .pre-commit-script.sh
+++ .pre-commit-script.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 echo "Executando script de pre-push de jhersonharyson..."
-
-commitHash=$(git rev-parse HEAD)
-echo "Commit Hash: $commitHash"
-echo "Project Name: $project_name"
-./upload.sh $project_name
+./upload.sh $1
